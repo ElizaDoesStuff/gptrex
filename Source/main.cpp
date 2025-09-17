d4dd31d442b41046e093395c9260c98ecce687ae
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-09-16 20:10:04 -0500

    Foundations of an NN!!

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream> 
+#include <string>
+
+#include "../Dependencies/Prettiest Print/prettiest.h"
+
+#include "Network.h"
+#include "Layer.h"
+#include "Neuron.h"
 
 int main() {
-	std::cout << "Hello, gptrex!" << std::endl;
-	return 0;
+	int layerSizes[] = { 16, 16, 16, 16 };
+	Network network(4, layerSizes);
+
+	log( "Network size: " + std::to_string(network.size) );
+	for (int i = 0; i < network.size; i++) {
+		log( "Layer " + std::to_string(i + 1) + " size: " + std::to_string(network.layers[i].size) );
+	}
 }
